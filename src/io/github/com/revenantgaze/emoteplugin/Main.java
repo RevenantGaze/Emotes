82962db610b798cd737297f624de9f952f260b9c
JonasFH <jonasfh@1337.no>
2014-02-02 21:26:43 +0100

    [Emotes - 1.6.2] Added framework for custom emotes
    Added framework for custom emotes, enabling/disabling of default emotes
    and config header

--- src/io/github/com/revenantgaze/emoteplugin/Main.java
+++ src/io/github/com/revenantgaze/emoteplugin/Main.java
@@ -1,5 +1,11 @@
 package io.github.com.revenantgaze.emoteplugin;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import io.github.com.revenantgaze.emoteplugin.commands.ArgueCmd;
 import io.github.com.revenantgaze.emoteplugin.commands.EmotesCmd;
 import io.github.com.revenantgaze.emoteplugin.commands.FacepalmCmd;
@@ -21,45 +27,129 @@ import io.github.com.revenantgaze.emoteplugin.unimplemented.EmoteCmd;
 
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 
 @SuppressWarnings("unused")
 public class Main extends JavaPlugin {
 
 	public Plugin instance;
 
+	public static ConfigManager cm;
+
+	File emoteConfigFile = new File(this.getDataFolder(), "emotes.yml");
+	FileConfiguration emoteConfig = YamlConfiguration
+			.loadConfiguration(emoteConfigFile);
+
 	@Override
 	public void onEnable() {
 
-		getCommand("emotes").setExecutor(new EmotesCmd(this));
-		getCommand("kiss").setExecutor(new KissCmd(this));
-		getCommand("hug").setExecutor(new HugCmd(this));
-		getCommand("wave").setExecutor(new WaveCmd(this));
-		getCommand("glomp").setExecutor(new GlompCmd(this));
-		getCommand("wink").setExecutor(new WinkCmd(this));
-		getCommand("ekick").setExecutor(new KickCmd(this));
-		getCommand("teabag").setExecutor(new TeabagCmd(this));
-		getCommand("flail").setExecutor(new FlailCmd(this));
-		getCommand("grumble").setExecutor(new GrumbleCmd(this));
-		getCommand("argue").setExecutor(new ArgueCmd(this));
-		getCommand("love").setExecutor(new LoveCmd(this));
-		getCommand("hate").setExecutor(new HateCmd(this));
-		getCommand("poke").setExecutor(new PokeCmd(this));
-		getCommand("smack").setExecutor(new SmackCmd(this));
-		getCommand("facepalm").setExecutor(new FacepalmCmd(this));
-		getCommand("whistle").setExecutor(new WhistleCmd(this));
-		//getCommand("emote").setExecutor(new EmoteCmd(this));
+		if (this.getConfig().getBoolean("use-default-emotes") == true) {
+
+			getCommand("emotes").setExecutor(new EmotesCmd(this));
+			getCommand("kiss").setExecutor(new KissCmd(this));
+			getCommand("hug").setExecutor(new HugCmd(this));
+			getCommand("wave").setExecutor(new WaveCmd(this));
+			getCommand("glomp").setExecutor(new GlompCmd(this));
+			getCommand("wink").setExecutor(new WinkCmd(this));
+			getCommand("ekick").setExecutor(new KickCmd(this));
+			getCommand("teabag").setExecutor(new TeabagCmd(this));
+			getCommand("flail").setExecutor(new FlailCmd(this));
+			getCommand("grumble").setExecutor(new GrumbleCmd(this));
+			getCommand("argue").setExecutor(new ArgueCmd(this));
+			getCommand("love").setExecutor(new LoveCmd(this));
+			getCommand("hate").setExecutor(new HateCmd(this));
+			getCommand("poke").setExecutor(new PokeCmd(this));
+			getCommand("smack").setExecutor(new SmackCmd(this));
+			getCommand("facepalm").setExecutor(new FacepalmCmd(this));
+			getCommand("whistle").setExecutor(new WhistleCmd(this));
+			getCommand("emote").setExecutor(new EmoteCmd(this));
+
+			getLogger().info("Default emotes is enabled!");
+
+		}
+
+		else {
+
+			getCommand("emotes").setExecutor(new EmotesCmd(this));
+			getCommand("emote").setExecutor(new EmoteCmd(this));
+
+			getLogger().info("Default emotes is disabled!");
+
+		}
 
 		FileConfiguration config = getConfig();
-		
-		if (!getDataFolder().isDirectory()) getDataFolder().mkdirs(); getConfig().options().copyDefaults(true); saveConfig();
 
-		config.options().copyDefaults(true);
-		
-		this.getConfig().addDefault("cooldown.default", 10);
-		this.getConfig().addDefault("emotes-distance", 40);
-		
+		getConfig().options().copyDefaults(true);
+
 		saveConfig();
+
+		File emoteConfigFile = new File(this.getDataFolder(), "emotes.yml");
+		FileConfiguration emoteConfig = YamlConfiguration
+				.loadConfiguration(emoteConfigFile);
+
+		boolean checkDefaultUse = this.getConfig().getBoolean(
+				"use-default-emotes");
+
+		if (!emoteConfigFile.exists()) {
+
+			emoteConfigFile = new File(getDataFolder(), "emotes.yml");
+
+			this.saveResource("emotes.yml", false);
+
+		}
+
+		this.getEmoteConfig()
+				.options()
+				.header("Custom emote configuration file\n"
+						+ "Set ''use-default-emotes'' to false in the main config if you do not want to have any other emotes than the ones you are adding yourself\n"
+						+ "All the emotes you create will be added to ''emotes-list'' and will be returned when doing /emote\n"
+						+ "--------------------------------------------------------------------------------------------------------\n"
+						+ "Sample setup:\n"
+						+ "When adding messages, start it from after the players name like shown below (sp-message and mp-message)\n"
+						+ "sp-message = single player message\n"
+						+ "--------------------------------------------------------------------------------------------------------\n"
+						+ "mp-message-bp = multiplayer message before target\n"
+						+ "Which is the message between the emote sender's name and the target player's name\n"
+						+ "--------------------------------------------------------------------------------------------------------\n"
+						+ "mp-message-ap = multiplayer message after target\n"
+						+ "Which is the message after the target player's name\n"
+						+ "--------------------------------------------------------------------------------------------------------\n"
+						+ "emotes:\n"
+						+ "  teabag:\n"
+						+ "    description: Teabag!\n"
+						+ "    usage: /emote teabag\n"
+						+ "    permission: emotes.commands.teabag\n"
+						+ "    sp-message: argues incoherently into the air!\n"
+						+ "    mp-message-bt: teabags\n"
+						+ "    mp-message-at: ! How embarassing!\n"
+						+ "--------------------------------------------------------------------------------------------------------\n"
+						+ "Emote without a target: <emote sender> <sp-message>\n"
+						+ "Emote with a target: <emote sender> <mp-message-bt> <target> <mp-message-at>\n"
+						+ "--------------------------------------------------------------------------------------------------------\n"
+						+ "According to this setup, this message will equal: <player> teabags <target>! How embarassing!)\n");
+
+		ConfigurationSection emoteSection = this.getEmoteConfig()
+				.getConfigurationSection("emotes");
+
+		if (emoteSection == null) {
+
+			emoteSection = this.getEmoteConfig().createSection("emotes");
+
+		}
+
+		ConfigurationSection emoteListSection = this.getEmoteConfig()
+				.getConfigurationSection("emotes-list");
+
+		if (emoteListSection == null) {
+
+			emoteListSection = this.getEmoteConfig().createSection(
+					"emotes-list");
+
+		}
+
+		this.saveEmoteConfig();
 
 		getLogger().info(
 				"Emotes v" + this.getDescription().getVersion()
@@ -71,6 +161,57 @@ public class Main extends JavaPlugin {
 	public void onDisable() {
 
 		getLogger().info("Emotes has been disabled!");
+
+	}
+
+	public void reloadEmoteConfig() {
+
+		if (emoteConfigFile == null) {
+
+			emoteConfigFile = new File(this.getDataFolder(), "emotes.yml");
+
+		}
+
+		emoteConfig = YamlConfiguration.loadConfiguration(emoteConfigFile);
+
+		InputStream emoteConfigStream = this.getResource("emotes.yml");
+
+		if (emoteConfigStream != null) {
+
+			YamlConfiguration blocksDefault = YamlConfiguration
+					.loadConfiguration(emoteConfigStream);
+			emoteConfig.setDefaults(blocksDefault);
+
+		}
+
+	}
+
+	public void saveEmoteConfig() {
+
+		try {
+
+			emoteConfig.save(emoteConfigFile);
+
+		}
+
+		catch (IOException ex) {
+
+			Logger.getLogger(JavaPlugin.class.getName()).log(Level.SEVERE,
+					"Could not save config to " + emoteConfigFile, ex);
+
+		}
+
+	}
+
+	public FileConfiguration getEmoteConfig() {
+
+		if (emoteConfig == null) {
+
+			this.reloadEmoteConfig();
+
+		}
+
+		return emoteConfig;
 
 	}
 
